a7a9fe04b71848e5c6fe1815c3143666b86a6010
songmin <tft0720@gmail.com>
2022-03-10 00:30:08 +0900

    useState

--- src/AppComponent.tsx
+++ src/AppComponent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useReducer, useRef, useState } from 'react';
+import React, { Dispatch, SetStateAction, useEffect, useMemo, useReducer, useRef, useState } from 'react';
 
 function Childone() {
   useEffect(() => {
@@ -133,6 +133,32 @@ function AppComponent() {
     setUsers(users.map((user) => (user.id === id ? { ...user, active: !user.active } : user)));
   };
 
+  /*
+  * Hook => useEffect, React는 브라우저가 모두 그려질 때까지 
+            useEffect의 수행을 지연하기때문에 다른 작업에 영향을 주지 않는다.
+            effect 함수 안에서 참조되는 모든 값은 의존성 값의 배열에 드러나야 한다.
+  */
+
+  /*
+  * Hook => useState,비동기, 여러번 호출하면 batch하여 state를 업데이트 한다.
+    매번 호출 순서대로 바로 업데이트하지 않고 인자로 전달된 객체들을 하나로 합치는 작업을 함(오브젝트 컴포지션)
+    함수형업데이트 => 객체형태가 아니라 함수로 전달하면 오브젝트 컴포지션을 하지 않고 호출된 순서대로 큐에 넣고
+    큐에 넣어진 대로 함수를 실행
+    https://darrengwon.tistory.com/788
+  */
+
+  // const onClickPlus = () => {
+  //   setCount(count + 1);
+  //   setCount(count + 1);
+  //   setCount(count + 1);
+  // };
+
+  const onClickPlus = () => {
+    setCount((curr) => curr + 1);
+    setCount((curr) => curr + 1);
+    setCount((curr) => curr + 1);
+  };
+
   return (
     <div>
       <Childone />
@@ -144,15 +170,14 @@ function AppComponent() {
           </option>
         ))}
       </select>
-      <form>
-        <input value={userName} type="text" placeholder="아이디" onChange={(e) => setUserName(e.currentTarget.value)} />
-        <input value={userEmail} type="text" placeholder="이메일" onChange={(e) => setUserEmail(e.currentTarget.value)} />
-      </form>
-      <UserList users={users} onToggle={onToggle} />
+      <MemoCreateUser userName={userName} setUserName={setUserName} userEmail={userEmail} setUserEmail={setUserEmail} />
+      <MemoUserList users={users} onToggle={onToggle} />
       <button ref={btnRef} onClick={clickHandleDispatch}>
         Dispatch
       </button>
       <div>활성사용자 수 : {activeCount}</div>
+      <h1>{count}</h1>
+      <button onClick={onClickPlus}>PLUS</button>
     </div>
   );
 }
@@ -182,3 +207,56 @@ function UserList({ users, onToggle }: { users: UsersProps[]; onToggle: (id: num
     </ul>
   );
 }
+
+// Hook => 특정 값을 비교해서 변경이 없다면(같다면) 리 렌더 하지 않음
+/*
+ * 컴퍼넌트가 React.memo()로 래핑 될 때, React는 컴퍼넌트를 렌더링하고 결과를 메모이징(Memoizing)한다. 
+   그리고 다음 렌더링이 일어날 때 props가 같다면, React는 메모이징(Memoizing)된 내용을 재사용한다.(가상DOM확인안함)
+   shallow 비교함
+ */
+
+/* function MovieViewsRealtime() {
+  title, 
+  releaseDate
+  views
+
+  주기적으로 서버에서 데이터를 가져와서 views를 업데이트 한다.
+  views 업데이트 될때마다 MovieViewsRealtime 컴포넌트 또한 리렌더링 한다.
+  이때 Moive 컴포넌트 또한 title, releaseDate 가 같음에도 불구하고 리렌더링 한다.
+  이럴때 React.memo 를 사용할 수 있다.
+  
+  return (
+    <div>
+      <Movie title={title} releaseDate={releaseDate} />
+      Movie views: {views}
+    </div>
+  );
+}
+*/
+
+// => 특정 props를 비교해서 렌더링을 컨트롤하는 것은 이후 버그를 발생시킬 수 있으므로 신중하게 한다.
+// onToggle에서 최신 users 배열을 참조하지 않으므로 심각한 오류가 발생 할 수 있습니다.
+// export const MemoUserList = React.memo(UserList, (preProps, nextProps) => preProps.users === nextProps.users);
+
+export const MemoUserList = React.memo(UserList);
+
+function CreateUser({
+  userName,
+  userEmail,
+  setUserName,
+  setUserEmail,
+}: {
+  userName: string;
+  userEmail: string;
+  setUserName: Dispatch<SetStateAction<string>>;
+  setUserEmail: Dispatch<SetStateAction<string>>;
+}) {
+  return (
+    <form>
+      <input value={userName} type="text" placeholder="아이디" onChange={(e) => setUserName(e.currentTarget.value)} />
+      <input value={userEmail} type="text" placeholder="이메일" onChange={(e) => setUserEmail(e.currentTarget.value)} />
+    </form>
+  );
+}
+
+export const MemoCreateUser = React.memo(CreateUser);
