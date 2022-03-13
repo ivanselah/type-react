33f53a2cd1c98611542cd4ff641260745a06aa0e
songmin <tft0720@gmail.com>
2022-03-13 18:02:10 +0900

    useReducer로 컴포넌트 내 state 밖으로 분리 로직 관리

--- src/project-1/AppCom.tsx
+++ src/project-1/AppCom.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useCallback, useEffect, useReducer, useRef, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import DiaryEditor, { StateProps } from './DiaryEditor';
 import DiaryList from './DiaryList';
@@ -17,8 +17,50 @@ type InitSateProps = {
   body: string;
 };
 
+type DataListAction = {
+  type: 'INIT' | 'CREATE' | 'DELETE';
+  data: StateAddDateProps | StateAddDateProps[] | StateProps;
+  targerId?: string;
+};
+
+function dataListReducer(state: StateAddDateProps[], action: DataListAction): StateAddDateProps[] {
+  switch (action.type) {
+    case 'INIT': {
+      const data = action.data as StateAddDateProps[];
+      return data;
+    }
+    case 'CREATE': {
+      if (action.targerId) {
+        const data = action.data as StateProps;
+        const modifiedData = state.map((item) => {
+          if (item.id === action.targerId) {
+            return {
+              ...item,
+              title: data.title,
+              content: data.content,
+              emotion: data.emotion,
+              created_date: new Date().getTime(),
+            };
+          } else {
+            return item;
+          }
+        });
+        return modifiedData;
+      }
+      const data = action.data as StateAddDateProps;
+      return [data, ...state];
+    }
+    case 'DELETE': {
+      return state.filter((data) => data.id !== action.targerId);
+    }
+    default:
+      return state;
+  }
+}
+
 function AppCom() {
-  const [dataList, setDataList] = useState<StateAddDateProps[]>([]);
+  // const [dataList, setDataList] = useState<StateAddDateProps[]>([]);
+  const [dataList, dataListDispatch] = useReducer(dataListReducer, []);
   const [modifyModalVisible, setModifyModalVisible] = useState(false);
   const [targetData, setTargetData] = useState<StateAddDateProps>();
   const targetId = useRef<string>();
@@ -42,7 +84,7 @@ function AppCom() {
       const randomEmotion = Math.floor(Math.random() * 5) + 1;
       initDataList.push(createNewObject({ title: data.email, content: data.body, emotion: randomEmotion }));
     });
-    setDataList(initDataList);
+    dataListDispatch({ type: 'INIT', data: initDataList });
   }, []);
 
   useEffect(() => {
@@ -58,31 +100,17 @@ function AppCom() {
     (data: StateProps) => {
       const newData = createNewObject(data);
       if (modifyModalVisible) {
-        setDataList((currList) => {
-          return currList.map((item) => {
-            if (item.id === targetId.current) {
-              return {
-                ...item,
-                title: data.title,
-                content: data.content,
-                emotion: data.emotion,
-                created_date: new Date().getTime(),
-              };
-            } else {
-              return item;
-            }
-          });
-        });
+        dataListDispatch({ type: 'CREATE', data, targerId: targetId.current });
         modifyVisibleToggle();
       } else {
-        setDataList((data) => [newData, ...data]);
+        dataListDispatch({ type: 'CREATE', data: newData });
       }
     },
     [modifyModalVisible]
   );
 
   const deleteData = useCallback((dataId: StateAddDateProps['id']) => {
-    setDataList((currList) => currList.filter((data) => data.id !== dataId));
+    dataListDispatch({ type: 'DELETE', data: [], targerId: dataId });
   }, []);
 
   const modifyVisibleToggle = () => {
@@ -95,23 +123,23 @@ function AppCom() {
     modifyVisibleToggle();
   }, []);
 
-  const getDiaryAnalysis = useMemo(() => {
-    const goodCount = dataList.filter((data) => data.emotion >= 3).length;
-    const badCount = dataList.length - goodCount;
-    const goodRatio = (goodCount / dataList.length) * 100;
-    return { goodCount, badCount, goodRatio };
-  }, [dataList]);
+  // const getDiaryAnalysis = useMemo(() => {
+  //   const goodCount = dataList.filter((data) => data.emotion >= 3).length;
+  //   const badCount = dataList.length - goodCount;
+  //   const goodRatio = (goodCount / dataList.length) * 100;
+  //   return { goodCount, badCount, goodRatio };
+  // }, [dataList]);
 
-  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+  // const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
 
   return (
     <div>
       <OptimizeTest />
       <DiaryEditor bringData={bringData} modifyModalVisible={modifyModalVisible} targetData={targetData!} />
-      <div>{dataList.length}</div>
+      {/* <div>{dataList.length}</div>
       <div>기분 좋은 일기 개수 : {goodCount}</div>
       <div>기분 나쁜 일기 개수 : {badCount}</div>
-      <div>기분 좋은 일기 비율 : {goodRatio}%</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}%</div> */}
       <DiaryList dataList={dataList} deleteData={deleteData} modifyData={modifyData} />
     </div>
   );
