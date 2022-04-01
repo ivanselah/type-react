329090feac3940366316de8eb36d4fdfde31ab25
songmin <tft0720@gmail.com>
2022-04-01 22:10:55 +0900

    redux-toolkit

---
+++ src/ReduxToolkit/store.js
@@ -0,0 +1,32 @@
+import { createStore } from 'redux';
+import { configureStore, createAction, createReducer, createSlice } from '@reduxjs/toolkit';
+
+/* const addToDo = createAction('ADD');
+const deleteToDo = createAction('DELETE');
+
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) => {
+    return state.filter((toDo) => toDo.id !== action.payload);
+  },
+});
+ */
+
+const toDo = createSlice({
+  name: 'toDosReducer',
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) => {
+      return state.filter((toDo) => toDo.id !== action.payload);
+    },
+  },
+});
+
+const store = configureStore({ reducer: toDo.reducer });
+
+export default store;
